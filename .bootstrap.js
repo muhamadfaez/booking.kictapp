26ad07ce1fcdd3f37e83b707f72531f15965031b
Vibesdk <vibesdk-bot@cloudflare.com>
2026-01-27 02:39:33 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add axios@^1.7.2"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
